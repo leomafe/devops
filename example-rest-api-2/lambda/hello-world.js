91b291797beab919558d54dd2336123cda39ffad
Leonardo Machado Fernandes <leo.machandes@gmail.com>
2023-08-17 20:33:24 -0300

    teste deploy

--- example-rest-api-2/lambda/hello-world.js
+++ example-rest-api-2/lambda/hello-world.js
@@ -4,7 +4,7 @@ exports.handler = async function(event, context) {
         statusCode: 200,
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-            message: "Olá Mundo!",
+            message: "Olá Mundo Novo!",
         }),
     };
 };
